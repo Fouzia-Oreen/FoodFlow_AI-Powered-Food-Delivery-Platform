46911bdc508886fce3f7e4054af87e9e5ea90e33
Fouzia Oreen <fouziaoreen@gmail.com>
2026-07-09 22:53:35 +0600

    readme updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Welcome to FoodFlow</h1>
+    <p>AI-Powered Food Delivery Platform</p>
+    <img src={heroImg} alt="Hero Image" className="hero-image" />
+    <h3 >
+      A Production-Ready Food Delivery Marketplace Built with the MERN Stack ecosystem featuring real-time order management, live Google Maps tracking, secure online payments, restaurant & delivery partner dashboards, AI-powered recommendations, and enterprise-grade administration
+    </h3>
+    </>
+  )
+}
+
+export default App
